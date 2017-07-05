a80c05c70cbd166731cf0491b31721bddfca3ac8
iDenis <denis.mitiliuc@gmail.com>
2017-07-05 17:06:08 +0200

    Add new images, board draw, player move

--- js/app.js
+++ js/app.js
@@ -1,5 +1,9 @@
+//Global variables
+var horisontal = 101, //use this to move horisontaly
+    vertical = 83;  //use this to move verticaly
+
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x, y, speed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
@@ -24,13 +28,48 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function(x, y) {
+  // Variables applied to each of our instances go here,
+  // we've provided one for you to get started
 
+  this.x = x * horisontal;
+  this.y = y * vertical;
+  // The image/sprite for our enemies, this uses
+  // a helper we've provided to easily load images
+  this.sprite = 'images/char-boy.png';
+};
+
+Player.prototype.update = function(dt) {
+    // You should multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for
+    // all computers.
+};
+
+// Draw the enemy on the screen, required method for game
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Player.prototype.handleInput = function(key) {
+  // var horisontal = 101,
+      // vertical = 83;
+  if (key === 'left' && this.x !== 0) {
+    this.x -= horisontal;
+  } else if (key === 'up' && this.y !== 0) {
+    this.y -= vertical;
+  } else if (key === 'right' && this.x !== horisontal * 4) {
+    this.x += horisontal;
+  } else if (key === 'down' && this.y !== vertical * 5) {
+    this.y += vertical;
+  }
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
-
+var allEnemies = [new Enemy(), new Enemy()];
+var player = new Player(0, 1);
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
