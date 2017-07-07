1ce76ab549623b60e7ce5fcae564e95d3486ff40
iDenis <denis.mitiliuc@gmail.com>
2017-07-07 12:00:54 +0200

    Super and Sub classes implementation

--- js/app.js
+++ js/app.js
@@ -1,54 +1,61 @@
-//Global variables
+// Global variables
 var horisontal = 101, //use this to move horisontaly
     vertical = 83;  //use this to move verticaly
 
-// Enemies our player must avoid
-var Enemy = function(x, y, speed) {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
+// Create SuperClass
+var Item = function(x , y, sprite) {
+  this.x = x;
+  this.y = y;
+  this.sprite = sprite;
 };
 
-// Update the enemy's position, required method for game
+// Update the items's position, required method for game
 // Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+Item.prototype.update = function(dt) {
+
 };
 
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+// Draw the item on the screen, required method for game
+Item.prototype.render = function() {
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+}
+
+// Create the bounding box
+Item.prototype.bBox = function() {
+
 };
+
+// This fucntion will create the new subclass object and constructor
+var createSubClass = function(subclass, superclass) {
+  subclass.prototype = Object.create(superclass.prototype);
+  subclass.prototype.constructor = subclass;
+};
+
+// Enemies our player must avoid
+var Enemy = function(x, y, sprite) {
+  Item.call(this, x, y, sprite);
+  // Variables applied to each of our instances go here,
+  // we've provided one for you to get started
+
+};
+
+// Create the Enemy object, assdf
+createSubClass(Enemy, Item);
 
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-var Player = function(x, y) {
+var Player = function(x, y, sprite) {
+  Item.call(this, x, y, sprite);
   // Variables applied to each of our instances go here,
   // we've provided one for you to get started
-
   this.x = x * horisontal;
   this.y = y * vertical;
   // The image/sprite for our enemies, this uses
   // a helper we've provided to easily load images
   this.sprite = 'images/char-boy.png';
 };
-
-Player.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-};
-
-// Draw the enemy on the screen, required method for game
-Player.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
+createSubClass(Player, Item);
 
 Player.prototype.handleInput = function(key) {
   // var horisontal = 101,
@@ -68,8 +75,8 @@ Player.prototype.handleInput = function(key) {
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
-var allEnemies = [new Enemy(), new Enemy()];
-var player = new Player(0, 1);
+var allEnemies = [new Enemy(0, 0, 'images/enemy-bug.png'), new Enemy(101, 83, 'images/enemy-bug.png')];
+var player = new Player(2, 5);
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
