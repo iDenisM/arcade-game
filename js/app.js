5e03ccb99914abe9ed541637b3db222d1ea1fcd7
Denis Mitiliuc <denismitiliuc@Deniss-MacBook-Pro.local>
2017-11-14 18:34:35 +0100

    Enemie move

--- js/app.js
+++ js/app.js
@@ -1,10 +1,25 @@
-// Enemies our player must avoid
-class Enemy {
+const horisontal = 101,
+      vertical = 80,
+      startEmeniesCount = 3;
+
+// Create super class for all game objects
+class GameObject {
   constructor(sprite) {
-    this.sprite = 'images/enemy-bug.png';
+    this.sprite = sprite;
   }
 
-  // Update the enemy's position, required method for game
+  // Draw the game object on the screen, required method for game
+  render() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  }
+
+  // Set the start position of the game object on the canvas
+  setStartPosition(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  // Update the game object's position, required method for game
   // Parameter: dt, a time delta between ticks
   update(dt) {
     // You should multiply any movement by the dt parameter
@@ -12,41 +27,58 @@ class Enemy {
     // all computers.
 
   }
+};
 
-  // Draw the enemy on the screen, required method for game
-  render() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+// Calculate random int value from min and max
+let randomIntFromInterval = (min,max) => {
+  return Math.floor(Math.random()*(max-min+1)+min);
+};
+
+// Enemies our player must avoid
+class Enemy extends GameObject {
+  constructor(sprite) {
+    sprite = 'images/enemy-bug.png';
+    super(sprite);
+    this.speed = randomIntFromInterval(100, 250);
+  }
+
+  update(dt) {
+    if (this.x <= 505) {
+      this.x += this.speed * dt;
+    }
+    else {
+      this.setStartPosition(-horisontal, vertical * randomIntFromInterval(1, 3))
+      this.speed = randomIntFromInterval(100, 250);
+    }
   }
 };
 
 // Now write your own player class
-class Player {
+class Player extends GameObject {
   constructor(sprite) {
-    this.sprite = 'images/char-boy.png'
+    sprite = 'images/char-boy.png';
+    super(sprite);
   }
 
-  update(dt) {
-
-  }
-
-  render() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-  }
-
+  // Handle the control of the movement of the player
   handleInput() {
 
   }
-}
+};
 
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 let allEnemies = [];
-allEnemies.push(new Enemy());
+// Create three enemies for the begining
+for (let i = 0; i < startEmeniesCount; i++) {
+  let enemy = new Enemy();
+  enemy.setStartPosition(0, vertical * randomIntFromInterval(1, 3));
+  allEnemies.push(enemy);
+};
 // Place the player object in a variable called player
 let player = new Player();
-player.x = 101;
-player.y = 101;
+player.setStartPosition(2 * horisontal, 3 * vertical);
 
 
 // This listens for key presses and sends the keys to your
